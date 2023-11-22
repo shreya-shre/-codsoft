fe79e745e529d9d2c3a9a1a6bea9e6a25115d1f3
shreya-shre <149089072+shreya-shre@users.noreply.github.com>
2023-11-22 06:45:52 -0800

    Add files via upload

---
+++ rock,paper,scisors game.py
@@ -0,0 +1,17 @@
+import random
+options =["Rock","Paper","Scissors"]
+user_choice=input("Choose Rock,Paper,or Scissors:")
+computer_choice=random.choice(options)
+print("You choose:",user_choice)
+print("Computer choose:",computer_choice)
+if user_choice==computer_choice:
+    print("It's a tie!")
+elif user_choice == "Rock"and computer_choice=="Scissors":
+    print("You win!")
+elif user_choice =="Paper"and computer_choice=="Rock":
+    print("You win!")
+elif user_choice =="Scissors"and computer_choice=="Paper":
+    print("You win!") 
+else:
+    print("Computer wins!")
+
